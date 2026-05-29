adfc120cf413fadbc214ce5af15b506dcd1d76fa
jiyxx <shuklajiya04@gmail.com>
2026-05-29 11:08:29 +0530

    Improved UI Layout

--- Week-3/src/UserData.jsx
+++ Week-3/src/UserData.jsx
@@ -3,13 +3,12 @@ import UserCard from "./components/UserCard";
 import "./App.css";
 
 export default function UserData() {
-  
   // State Management
-  const[user , setUser] = useState([]);
-  const[loading , setLoading] = useState(true);
-  const[error , setError] = useState("");
-  const[search , setSearch] = useState("");
- 
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [search, setSearch] = useState("");
+
   // Fetching Data from API
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -25,37 +24,37 @@ export default function UserData() {
         setLoading(false);
       });
   }, []);
-  
+
   //Filtering Data based on search input
   const filteredUsers = user.filter(
     (user) =>
       user.name.toLowerCase().includes(search.toLowerCase()) ||
       user.email.toLowerCase().includes(search.toLowerCase()) ||
-      user.address.city.toLowerCase().includes(search.toLowerCase())
+      user.address.city.toLowerCase().includes(search.toLowerCase()),
   );
 
-  //Rendering the component 
+  //Rendering the component
   return (
     //Main Container
     <div className="container">
+      <header className="header">
+        <h1>User Directory</h1>
 
-      <h1>User Directory</h1>
-    
-      <div className="search-box">
-        <input
-          type="text"
-          className="input"
-          placeholder="Search by name, email or city..."
-          value={search}
-          onChange={(event) => setSearch(event.target.value)}
-        />
-      </div>
-  
+        <div className="search-box">
+          <input
+            type="text"
+            className="input"
+            placeholder="Search by name, email or city..."
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+        </div>
+      </header>
       <div className="display">
         {loading && <p>Loading...</p>}
         {error && <p>Error: {error}</p>}
       </div>
-      
+
       <div className="user-grid">
         {filteredUsers.map((user) => (
           <UserCard
@@ -69,10 +68,9 @@ export default function UserData() {
         ))}
       </div>
 
-      <div className = "user-grid">
+      <div className="user-grid">
         {filteredUsers.length === 0 && !loading && <p> No Users found</p>}
       </div>
-
     </div>
   );
 }
