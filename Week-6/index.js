a7fc5f1abc1c7c00900929bc11d3eed1b3e08ed1
jiyxx <shuklajiya04@gmail.com>
2026-06-19 17:17:38 +0530

    Middleware Fixed

--- Week-6/index.js
+++ Week-6/index.js
@@ -3,19 +3,36 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/users");
+const logger = require("./middleware/logger");
 
 const app = express();
 
+
 connectDB();
 
+// Middleware
 app.use(express.json());
+app.use(logger);
 
-app.use("/users", userRoutes);
+// Routes
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
 
-app.listen(3000, () => {
-  console.log("Server Started");
+// Centralized Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(500).json({
+    message: "Something went wrong"
+  });
+});
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server Started on Port ${PORT}`);
 });
